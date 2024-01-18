497d0c211522adb0bdb26b67c95c7de397ba3821
Henri Patsi <hpatsi@student.hive.fi>
2024-01-18 09:15:40 +0200

    updated tester

--- test/tester.sh
+++ test/tester.sh
@@ -12,18 +12,47 @@ YELLOW='\033[0;33m'
 HEADER_COLOR=$LCYAN
 COMMAND_COLOR=$CYAN
 
+TEST_DIR=./tests
+SRCS_DIR=./srcs
+
+#------ BUILT-IN ------#
+
+printf $HEADER_COLOR"\n#------ BUILT-IN ------#\n\n"$NC
+
+while read -r line; do
+	echo -e $COMMAND_COLOR $line $NC
+	eval $line
+done < $TEST_DIR/built_in_tests.txt
+
 #------ BASIC ------#
 
 printf $HEADER_COLOR"\n#------ BASIC ------#\n\n"$NC
 
-echo -e $COMMAND_COLOR"echo hello"$NC
-echo hello
+while read -r line; do
+	echo -e $COMMAND_COLOR $line $NC
+	eval $line
+done < $TEST_DIR/basic_tests.txt
+
+#------ PIPES ------#
+
+printf $HEADER_COLOR"\n#------ PIPES ------#\n\n"$NC
+
+while read -r line; do
+	echo -e $COMMAND_COLOR $line $NC
+	eval $line
+done < $TEST_DIR/pipe_tests.txt
 
 #------ && AND || ------#
 
 printf $HEADER_COLOR"\n#------ && AND || ------#\n\n"$NC
 
+cc $SRCS_DIR/true.c -o true
+cc $SRCS_DIR/false.c -o false
+
 while read -r line; do
 	echo -e $COMMAND_COLOR $line $NC
 	eval $line
-done < "and_or_tests.txt"
+done < $TEST_DIR/and_or_tests.txt
+
+rm true
+rm false

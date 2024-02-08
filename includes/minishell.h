02ef798ade770c87f865c05e59664b1b22b01409
Taavi Oramo <toramo@student.hive.fi>
2024-02-08 13:59:02 +0200

    fix ft_cd args

--- includes/minishell.h
+++ includes/minishell.h
@@ -55,7 +55,7 @@ int		cmd_line_error(t_vec *cmd_lines);
 int		prepare_cmd(t_cmd_line *cmd_line, int *last_return);
 
 int		ft_echo(t_vec *argv);
-int		ft_cd(t_vec *argv);
+int		ft_cd(t_vec *argv, t_vec *env);
 int		ft_pwd(t_vec *argv);
 int		ft_env(t_vec *env);
 int		ft_export(t_vec *argv, t_vec *env);

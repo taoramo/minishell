55ead8230348a334dbd0ade39bba9aa26f7be9d4
Henri Patsi <hpatsi@student.hive.fi>
2024-02-12 13:27:24 +0200

    fixed pipe $? handling

--- srcs/commands/pipex.c
+++ srcs/commands/pipex.c
@@ -6,7 +6,7 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:13:47 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/02/12 10:17:51 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/02/12 13:25:29 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,7 @@ int	prepare_pipe(t_vec *commands, char *pipe_str, t_vec *env,  int last_return)
 	char		**strs;
 	t_command	*command;
 	int			i;
-	int			exit_code;
 
-	exit_code = 0;
 	strs = ft_split(pipe_str, '|');
 	if (strs == 0)
 		return (0);
@@ -41,22 +39,21 @@ int	prepare_pipe(t_vec *commands, char *pipe_str, t_vec *env,  int last_return)
 	while (strs[i] != 0)
 	{
 		command = ft_calloc(1, sizeof(t_command));
-		exit_code = prepare_command(command, strs[i], env, last_return);
+		last_return = prepare_command(command, strs[i], env, last_return);
 		vec_push(commands, command);
 		i++;
 	}
-	return (exit_code);
+	return (last_return);
 }
 
 int	pipex(char *pipe_str, t_vec *env, int last_return)
 {
 	t_vec	commands;
 	int		*process_ids;
-	int		exit_code;
 	int		ret;
 
 	vec_new(&commands, 1, sizeof(t_command));
-	exit_code = prepare_pipe(&commands, pipe_str, env, last_return);
+	last_return = prepare_pipe(&commands, pipe_str, env, last_return);
 	process_ids = ft_calloc(commands.len + 1, sizeof(int));
 	if (process_ids == 0)
 		return (-1);
@@ -66,8 +63,8 @@ int	pipex(char *pipe_str, t_vec *env, int last_return)
 		return (-1);
 	}
 	ret = wait_for_children(process_ids);
-	if (exit_code == 0)
-		exit_code = ret;
+	if (last_return == 0)
+		last_return = ret;
 	free(process_ids);
-	return (exit_code);
+	return (last_return);
 }

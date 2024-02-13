916977f8cb0d0863dcb572e82614821d70b76e6f
Henri Pätsi <patsihenri@gmail.com>
2024-02-13 18:03:24 +0200

    pipex working with refactor

--- srcs/commands/pipe_commands.c
+++ srcs/commands/pipe_commands.c
@@ -6,7 +6,7 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 16:15:37 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/02/13 11:46:05 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/02/13 17:54:59 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,57 +41,27 @@ void	handle_parent(int pos, int pipe_fds[], int pipe2_fds[])
 	}
 }
 
-void	handle_child(t_command *command, int pipe_fds[], int pos)
+void	handle_child(t_command *command, int pipe_fds[], int pipe2_fds[], int pos)
 {
-	ft_printf("pos = %d, executing\n", pos);
-	vec_iter(&command->argv, vec_print_elem_str);
-
 	if (pos == 0)
 	{
 		close(pipe_fds[0]);
 		apply_pipe_redirect(command, 0, pipe_fds[1]);
 	}
 	else if (pos == 1)
+	{
 		apply_pipe_redirect(command, pipe_fds[0], 1);
-	// else
-	// {
-	// 	close(pipe2_fds[0]);
-	// 	apply_pipe_redirect(command, pipe_fds[0], pipe2_fds[1]);
-	// }
-	
+	}
+	else
+	{
+		close(pipe2_fds[0]);
+		apply_pipe_redirect(command, pipe_fds[0], pipe2_fds[1]);
+	}
 	execute_command(command->argv, command->env);
 	exit (1);
 }
 
-// int	pipe_command(t_command *command, int pos, int pipe_fds[])
-// {
-// 	
-// 	int		pipe2_fds[2];
-
-// 	if (pos == 2 && pipe(pipe2_fds) < 0)
-// 	{
-// 		perror("pipe failed");
-// 		return (-1);
-// 	}
-// 	if (command->argv.len != 0)
-// 	{
-// 		if (builtin_index(*(char **)vec_get(&command->argv, 0)) != -1)
-// 			return (run_builtin_pipe(command, pos, pipe_fds, pipe2_fds));
-// 	}
-// 	process_id = fork();
-// 	if (process_id < 0)
-// 	{
-// 		perror("fork failed");
-// 		return (-1);
-// 	}
-// 	else if (process_id > 0)
-// 		handle_parent(pos, pipe_fds, pipe2_fds);
-// 	else
-// 		handle_child(command, pos, pipe_fds, pipe2_fds);
-// 	return (process_id);
-// }
-
-int	run_single_pipe_command(t_command *command, int pipe_fds[], int pos)
+int	run_single_pipe_command(t_command *command, int pipe_fds[], int pipe2_fds[], int pos)
 {
 	command->process_id = fork();
 	if (command->process_id < 0)
@@ -100,9 +70,9 @@ int	run_single_pipe_command(t_command *command, int pipe_fds[], int pos)
 		return (-1);
 	}
 	else if (command->process_id == 0)
-		handle_child(command, pipe_fds, pos);
+		handle_child(command, pipe_fds, pipe2_fds, pos);
 	else
-		handle_parent(pos, pipe_fds, 0);
+		handle_parent(pos, pipe_fds, pipe2_fds);
 	return (command->process_id);
 }
 
@@ -116,7 +86,7 @@ int first_pipe_commmand(char *str, int pipe_fds[], t_vec *env, int last_return)
 		if (builtin_index(*(char **)vec_get(&command.argv, 0)) != -1)
 			return (run_builtin_pipe(&command, 0, pipe_fds, 0));
 	}
-	return (run_single_pipe_command(&command, pipe_fds, 0));
+	return (run_single_pipe_command(&command, pipe_fds, 0, 0));
 }
 
 int last_pipe_command(char *str, int pipe_fds[], t_vec *env)
@@ -124,7 +94,31 @@ int last_pipe_command(char *str, int pipe_fds[], t_vec *env)
 	t_command	command;
 	
 	prepare_command(&command, str, env, 0);
-	return (run_single_pipe_command(&command, pipe_fds, 1));
+	if (command.argv.len != 0)
+	{
+		if (builtin_index(*(char **)vec_get(&command.argv, 0)) != -1)
+			return (run_builtin_pipe(&command, 1, pipe_fds, 0));
+	}
+	return (run_single_pipe_command(&command, pipe_fds, 0, 1));
+}
+
+int	middle_pipe_command(char *str, int pipe_fds[], t_vec *env)
+{
+	int			pipe2_fds[2];
+	t_command	command;
+
+	if (pipe(pipe2_fds) < 0)
+	{
+		perror("pipe failed");
+		return (-1);
+	}
+	prepare_command(&command, str, env, 0);
+	if (command.argv.len != 0)
+	{
+		if (builtin_index(*(char **)vec_get(&command.argv, 0)) != -1)
+			return (run_builtin_pipe(&command, 2, pipe_fds, pipe2_fds));
+	}
+	return (run_single_pipe_command(&command, pipe_fds, pipe2_fds, 2));
 }
 
 int	pipe_commands(char **strs, int **p_ids, t_vec *env, int last_return)
@@ -145,7 +139,7 @@ int	pipe_commands(char **strs, int **p_ids, t_vec *env, int last_return)
 		else if (i == count_commands(strs) - 1)
 			(*p_ids)[i] = last_pipe_command(strs[i], pipe_fds, env);
 		else
-			continue ;
+			(*p_ids)[i] = middle_pipe_command(strs[i], pipe_fds, env);
 		if ((*p_ids)[i] == -1)
 			return (-1);
 		i++;
--- srcs/commands/pipex.c
+++ srcs/commands/pipex.c
@@ -6,19 +6,19 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:13:47 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/02/13 11:47:18 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/02/13 17:16:18 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commands.h"
 
-static int	wait_for_children(int *process_ids)
+static int	wait_for_children(int *process_ids, int len)
 {
 	int	i;
 	int	ret;
 
 	i = 0;
-	while (process_ids[i] != 0)
+	while (i < len)
 	{
 		waitpid(process_ids[i], &ret, 0);
 		i++;
@@ -57,20 +57,12 @@ int	pipex(char *pipe_str, t_vec *env, int last_return)
 	process_ids = ft_calloc(count_commands(strs) + 1, sizeof(int));
 	if (process_ids == 0)
 		return (-1);
-		
-	pipe_commands(strs, &process_ids, env, last_return);
-	
-	// vec_new(&commands, 1, sizeof(t_command));
-	// last_return = prepare_pipe(&commands, pipe_str);
-
-	// if (pipe_commands(commands, &process_ids) == -1)
-	// {
-	// 	free(process_ids);
-	// 	return (-1);
-	// }
-	ret = wait_for_children(process_ids);
-	// if (last_return == 0)
-	// 	last_return = ret;
+	if (pipe_commands(strs, &process_ids, env, last_return) == -1)
+	{
+		free(process_ids);
+		return (-1);
+	}
+	ret = wait_for_children(process_ids, count_commands(strs));
 	free(process_ids);
 	return (ret);
 }

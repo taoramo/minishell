509bbf39e8d364afc832024d0b6b109f894fa498
Henri Pätsi <patsihenri@gmail.com>
2024-02-21 20:01:25 +0200

    free on fail to expand env

--- srcs/expand/expand_envs.c
+++ srcs/expand/expand_envs.c
@@ -6,7 +6,7 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 13:31:19 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/02/20 12:04:30 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/02/21 15:00:30 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int	expand_substr_env(t_vec *str_vec, size_t *i, t_vec *env, int last_return)
 	char	*env_expanded;
 	int		ret;
 	char	*str;
-
+	
 	str = (char *) vec_get(str_vec, 0);
 	if (*i + 1 >= str_vec->len || str[*i + 1] == 0 || ft_isspace(str[*i + 1]))
 		return (1);
@@ -116,7 +116,11 @@ int	expand_envs(t_vec *argv, t_vec *env, int last_return)
 			if (vec_from(&str_vec, *str_ptr,
 					ft_strlen(*str_ptr) + 1, sizeof(char)) < 0)
 				return (-1);
-			expand_str_envs(str_ptr, env, last_return, &str_vec);
+			if (expand_str_envs(str_ptr, env, last_return, &str_vec) == -1)
+			{
+				vec_free(&str_vec);
+				return (-1);
+			}
 		}
 		i++;
 	}
